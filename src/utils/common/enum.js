ef44225e633295eb5654c0b89a87cb7da3bb0097
Mohit <dhanwaniap@gmail.com>
2025-10-18 11:15:52 +0530

    added booking model & utils folder from flight service

---
+++ src/utils/common/enum.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    BUSINESS: "Business",
+    ECONOMY: "Economy",
+    PREMIUM_ECONOMY: "Premium_Economy",
+    FIRST_CLASS: "First_Class",
+}
+
+const BOOKING_STATUS = {
+    BOOKED: "Booked",
+    CANCELLED: "Cancelled",
+    INITIATED: "Initiated",
+    PENDING: "Pending",
+}
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS,
+}; 
